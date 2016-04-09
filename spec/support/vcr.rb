d451ce9ad5987d88affda6d3b6de3b00c21037e9
Kel Kanhirun <kel.kanhirun@gmail.com>
2016-04-09 11:49:16 +0700

    Allows http connections to be made when there is no cassette

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,6 +1,7 @@
 VCR.configure do |config|
 	config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
 	config.hook_into :webmock
+  config.allow_http_connections_when_no_cassette = true
 
 	# Use `:vcr` metadata to record an HTTP request, or
 	# Pass a hash to set the cassette options:
