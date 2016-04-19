90aafc29649d9cd62950672cb213e70096ca5492
Kel Kanhirun <kel.kanhirun@gmail.com>
2016-04-19 10:35:12 -0500

    Strip indentation in heredocs with strip_heredoc()

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,9 @@ SimpleCov.start 'rails'
 PROJECT_ROOT = File.expand_path('../..', __FILE__)
 $:.unshift PROJECT_ROOT
 
+# Strip indentation in heredocs:
+require 'active_support/core_ext/string/strip'
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
