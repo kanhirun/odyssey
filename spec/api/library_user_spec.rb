8d2d30949e5859fe9237a54241d8cb8bfdea8a22
Kel Kanhirun <kel.kanhirun@gmail.com>
2016-04-30 19:50:06 -0500

    Refactor: Uses creational method to hide construction details

--- spec/api/library_user_spec.rb
+++ spec/api/library_user_spec.rb
@@ -8,28 +8,17 @@ RSpec.describe 'API', type: :request do
   describe 'POST /api/library_users' do
     it 'creates a new LibraryUser' do
       expect do
-        post '/api/library_users', library_user: { first_name: 'Kevin',
-                                                   last_name: 'Kanhirun',
-                                                   phone_number: '8479701783',
-                                                   address_1: '424 W. Diversey Pkwy',
-                                                   zip_code: '60614',
-                                                   email: 'kel.kanhirun@gmail.com',
-                                                   password: 'password',
-                                                   library_card_number: '234942184560' }
+        post '/api/library_users', library_user: Factory.attributes_for(:library_user)
       end.to change{ LibraryUser.count }.by 1
 
       expect(response).to have_http_status :created
+      require 'pry'; binding.pry
       expect(response.content_type.to_s).to eq 'application/json'
     end
 
     it 'fails to create a user when missing a required field' do
       expect do
-        post '/api/library_users', library_user: { first_name: 'Kevin',
-                                                   last_name: 'Kanhirun',
-                                                   phone_number: '8479701783',
-                                                   zip_code: '60614',
-                                                   password: 'password',
-                                                   library_card_number: '234942184560' }
+        post '/api/library_users', library_user: Factory.attributes_for(:library_user, email: nil)
       end.not_to change{ LibraryUser.count }
 
       expect(response).to have_http_status :bad_request
